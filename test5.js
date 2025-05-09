8481297f4281d6e1e114bb0d30ed2665eaddb2c0
HTN1985-HPT <nagraja_ht@yahoo.com>
2025-05-09 20:13:34 +0530

    Add files via upload

---
+++ test5.js
@@ -0,0 +1,10 @@
+
+function fun2(){
+var ht= document.getElementById("myText").value;
+if(ht>=18){
+alert("you are eligible for vote");
+}
+else if(ht<18){
+    alert("your Not Eligible for vote");
+}
+}
